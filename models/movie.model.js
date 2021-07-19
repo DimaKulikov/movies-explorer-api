8334b240a04512334d26a2d11cff3acf822e7271
DimaKulikov <dmyukulikov@gmail.com>
2021-07-19 08:42:07 +0300

    add mongoose models

---
+++ models/movie.model.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const urlValidator = require('../utils/utils');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: urlValidator,
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: urlValidator,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: urlValidator,
+    },
+  },
+  owner: {
+    type: String,
+    required: true,
+    validate: (input) => validator.isMongoId(input),
+  },
+  movieID: {
+    type: String,
+    require: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
